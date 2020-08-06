ed31cc3f8530449478865d267a92993d75b69dab
Sylvia-ankamah <sankamah@mun.ca>
2020-08-06 00:00:31 -0230

    Trendline plot on births

---
+++ Trendline_plot1.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+Birth_data = pd.read_csv("daily-total-female-births.csv",parse_dates=['Date'],index_col='Date')  #Birth_data
+data = Birth_data
+
+fig, ax = plt.subplots(figsize=(12, 8))
+res = mk.original_test(data)
+trend_line = np.arange(len(data)) * res.slope + res.intercept
+
+ax.plot(data)
+ax.plot(data.index, trend_line)
+ax.legend(['data', 'trend line'])
+ax.set(xlabel= "Dates", ylabel= "Births", title="Trend line")
+fig.savefig('Trendline_plot1.png')
