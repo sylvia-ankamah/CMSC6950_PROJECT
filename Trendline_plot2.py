513d3864a60f84633d9bd0073872cd2e466b7916
Sylvia-ankamah <sankamah@mun.ca>
2020-08-06 00:01:01 -0230

    Trendline plot on Shampoo sales

---
+++ Trendline_plot2.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt                                                                                                                                                                                                                                                                                                                 Shampoo_data=pd.read_csv("shampoo.csv", parse_dates=['Month'],index_col='Month')
+ data = Shampoo_data
+
+ fig, ax = plt.subplots(figsize=(12, 8))
+ res = mk.hamed_rao_modification_test(data)
+ trend_line = np.arange(len(data)) * res.slope + res.intercept
+
+ ax.plot(data)
+ ax.plot(data.index, trend_line)
+ ax.legend(['data', 'trend line'])
+ ax.set(xlabel= "Months", ylabel= "Sales", title="TREND LINE")
+ fig.savefig('SHAMPOO_DATA_TRENDLINE.png')
