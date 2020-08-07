ca8926e1f55525a7f22c0434b6a2bf924410ffb5
Sylvia-ankamah <sankamah@mun.ca>
2020-08-07 18:25:17 -0230

    Trendline on Shampoo sales

--- Trendline_plot2.py
+++ Trendline_plot2.py
@@ -1,14 +1,17 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt                                                                                                                                                                                                                                                                                                                 Shampoo_data=pd.read_csv("shampoo.csv", parse_dates=['Month'],index_col='Month')
- data = Shampoo_data
+import matplotlib.pyplot as plt
+import pymannkendall as mk
 
- fig, ax = plt.subplots(figsize=(12, 8))
- res = mk.hamed_rao_modification_test(data)
- trend_line = np.arange(len(data)) * res.slope + res.intercept
+Shampoo_data=pd.read_csv("shampoo.csv", parse_dates=['Month'],index_col='Month')
+data = Shampoo_data
 
- ax.plot(data)
- ax.plot(data.index, trend_line)
- ax.legend(['data', 'trend line'])
- ax.set(xlabel= "Months", ylabel= "Sales", title="TREND LINE")
- fig.savefig('SHAMPOO_DATA_TRENDLINE.png')
+fig, ax = plt.subplots(figsize=(12, 8))
+res = mk.hamed_rao_modification_test(data)
+trend_line = np.arange(len(data)) * res.slope + res.intercept
+
+ax.plot(data)
+ax.plot(data.index, trend_line)
+ax.legend(['data', 'trend line'])
+ax.set(xlabel= "Months", ylabel= "Sales", title="TREND LINE")
+fig.savefig('SHAMPOO_DATA_TRENDLINE.png')
