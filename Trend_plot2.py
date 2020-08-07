1afe2f5ff8f83cf01a60560c638d5eadef4f16d0
Sylvia-ankamah <sankamah@mun.ca>
2020-08-07 18:29:53 -0230

    Trend on Shampoo sales data

--- Trend_plot2.py
+++ Trend_plot2.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 
 
-Shampoo_data=pd.read_csv("shampoo.csv", parse_dates=['Month'],index_col='Month'
-Trend_plot2=Shampoo_data.plot(figsize=(12,8));
+Shampoo_data=pd.read_csv("shampoo.csv", parse_dates=['Month'],index_col='Month')
+fig_2=Shampoo_data.plot(figsize=(12,8));
 plt.title=('Trend chart')
-fig.figure.savefig('Shampoo_data_Trend_plot.png')
+fig_2.figure.savefig('Shampoo_data_Trend_plot.png')
