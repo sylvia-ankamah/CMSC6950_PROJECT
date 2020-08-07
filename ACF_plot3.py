571533080712dd02199401f5140ace9d0b090bc7
Sylvia-ankamah <sankamah@mun.ca>
2020-08-07 18:26:39 -0230

    ACF ON MALARIA DATA

--- ACF_plot3.py
+++ ACF_plot3.py
@@ -2,7 +2,8 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 
-Malaria_data = pd.read_csv("Malaria_data.csv",parse_dates=['Date'],index_col='Date')
-fig, ax = plt.subplots(figsize=(12, 8))                                                                                                                                 sm.graphics.tsa.plot_acf(Malaria_data, lags=20, ax=ax);                                                                                                                 ax.set(xlabel= "Years", ylabel= "Malaria cases", title="AUTOCORRELATION")                                                                                               fig.savefig('MALARIA_DATA_ACF_PLOT.png') 
+Malaria_data = pd.read_csv("Malaria_data.csv")
+fig, ax = plt.subplots(figsize=(12, 8))                                                                                 sm.graphics.tsa.plot_acf(Malaria_data, lags=20, ax=ax);                                                                 ax.set(xlabel= "Years", ylabel= "Malaria cases", title="AUTOCORRELATION")                                               fig.savefig('MALARIA_DATA_ACF_PLOT.png') 
