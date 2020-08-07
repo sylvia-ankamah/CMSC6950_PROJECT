9fe0daef705a6d636f22501616961e6c560c3298
Sylvia-ankamah <sankamah@mun.ca>
2020-08-07 18:25:58 -0230

    ACF ON BIRTH DATA

--- ACF_plot1.py
+++ ACF_plot1.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 Birth_data = pd.read_csv("daily-total-female-births.csv",parse_dates=['Date'],index_col='Date')  #Birth_data
 fig, ax = plt.subplots(figsize=(12, 8))
